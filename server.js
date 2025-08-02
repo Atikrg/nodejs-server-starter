ca5eb5d9d9b1e25a2230751578273a5444da5e87
atikrangnekar <atikrangnekar28@gmail.com>
2025-08-02 13:38:36 +0530

    created starter project

---
+++ server.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+require("dotenv").config()
+const port = process.env.SERVER_PORT
+
+app.listen(port, ()=> {
+    console.log("SERVER LISTENING AT PORT - ",port);
+})
+
---
+++ utils/mailUtils.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER, // Your email address
+    pass: process.env.EMAIL_PASS, // App password, not your normal password
+  },
+});
+
+const sendMail = async (to, subject, text, html = "") => {
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text,
+      html,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.response);
+    return info;
+  } catch (err) {
+    console.error("Error sending email:", err);
+    throw err;
+  }
+};
+
+module.exports = { sendMail };
